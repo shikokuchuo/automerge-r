ba11675ad93ba46395a78f1e63b959ff7f2d44a8
shikokuchuo <53399081+shikokuchuo@users.noreply.github.com>
2025-11-22 23:10:56 +0000

    automerge-c: remove cli, test, wasm, edit-trace

--- src/automerge/rust/automerge-wasm/deno-tests/deno.ts
+++
@@ -1,8 +0,0 @@
-// @deno-types="../index.d.ts"
-import { create } from '../deno/automerge_wasm.js'
-
-Deno.test("It should create, clone and free", () => {
-  const doc1 = create()
-  const doc2 = doc1.clone()
-  doc2.free()
-});
--- src/automerge/rust/automerge-wasm/src/export-web-wasm.js
+++
@@ -1,6 +0,0 @@
-// This file is inserted into ./web by the build script
-
-import WASM from "./automerge_wasm_bg.wasm";
-import { initSync } from "./automerge_wasm.js";
-initSync(WASM);
-export * from "./automerge_wasm.js";
